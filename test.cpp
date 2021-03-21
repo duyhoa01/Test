cee0b57078ec34eae5c169a0057097216a2a606a
= <=>
2021-03-21 14:39:10 +0700

    them ham kt nt

--- test.cpp
+++ test.cpp
@@ -1,6 +1,13 @@
 <<<<<<< HEAD:test.cpp
 #include <stdio.h>
-
+int Nt(int n)
+{
+	if(n<2) return 0;
+	for(int i=2;i<n-1;i++) {
+		if(n%i==0) return 0;
+	}
+	return 1;
+}
 main()
 {
 	printf("hello world");
